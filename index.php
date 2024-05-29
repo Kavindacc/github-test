f23c4dffdf013a58dfa3e3efe3eb3c09b0d3a614
Kavindacc <kavindacc@gmail.com>
2024-05-29 15:26:51 +0530

    lign

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 ?>
 
 <html>
-<<<<<<< HEAD
     <h1>hasdasdasd asdasdasd</h1>
     <asdasd
 
@@ -13,8 +12,6 @@
 
 
     asdasdasd
-=======
     <h1>hasdasdasd</h1>
     <h1>login<h1>
->>>>>>> 6bc60d106dedea64e5ccc6e58e36a67d413c4a25
 </html>
